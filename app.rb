8280a79fc50e331044b3a7cf63e252051f2f0091
Wang Qun Feng <qunfengw@gmail.com>
2016-05-22 20:49:43 +0800

    refactoring:move the Date parser to proper classes

--- app.rb
+++ app.rb
@@ -26,8 +26,8 @@ list.all
 # SHOULD CREATE AN UNTITLED LIST AND ADD ITEMS TO IT
 # --------------------------------------------------
 new_list = UdaciList.new # Should create a list called "Untitled List"
-new_list.add("todo", "Buy more dog food", due: Chronic.parse("in 5 weeks").to_s, priority: "medium")
-new_list.add("todo", "Go dancing", due: Chronic.parse("in 2 hours").to_s)
+new_list.add("todo", "Buy more dog food", due: "in 5 weeks", priority: "medium")
+new_list.add("todo", "Go dancing", due: "in 2 hours")
 new_list.add("todo", "Buy groceries", priority: "high")
 new_list.add("event", "Birthday Party", start_date: "May 31")
 new_list.add("event", "Vacation", start_date: "Dec 20", end_date: "Dec 30")
--- lib/todo.rb
+++ lib/todo.rb
@@ -6,7 +6,7 @@ class TodoItem
 
   def initialize(description, options={})
     @description = description
-    @due = options[:due] ? Date.parse(options[:due]) : options[:due]
+    @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
     @type = "todo"
     if @priority and !(@@supported_priorities.include?(@priority))
